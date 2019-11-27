23a8dd47e3acfa3bf5eecd26d1cd06015875721d
nitin shukla <nshukla6@gmail.com>
2019-11-27 13:11:08 +0530

    Updates

---
+++ precache-manifest.57a42bc79ad9db49d9b599745b7960fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c9823ba12a7891f3c72884b1895885f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "0ea01c2e0c22d47dabb4",
+    "url": "/monsters-rolodex/static/css/main.865e7c75.chunk.css"
+  },
+  {
+    "revision": "3a057e02db196afa0ca3",
+    "url": "/monsters-rolodex/static/js/2.f833121f.chunk.js"
+  },
+  {
+    "revision": "0ea01c2e0c22d47dabb4",
+    "url": "/monsters-rolodex/static/js/main.62de820b.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
